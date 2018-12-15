3f6230d0f5d865f9dc78ccf18f6abfb9bbd2252a
LinQingWei <1441779217@qq.com>
2018-12-15 15:47:24 +0800

    SMS: init.

---
+++ app/src/main/java/com/freeme/sms/MainActivity.java
@@ -0,0 +1,12 @@
+package com.freeme.sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
